0a5da368ff67c7a84b2b560efae9bcf6e7bee9bc
Lnussbaumer <linda.nussbaumer@students.fh-wels.at>
2024-11-26 08:32:44 +0100

    reset

--- inputfilereader/inputfilereader.py
+++ inputfilereader/inputfilereader.py
@@ -1,32 +1,35 @@
+
 import mbsObject
 import json
 
 f = open("inputfilereader/test.fdd","r")
 
 fileContent = f.read().splitlines()
-f.close()                               # braucht man nicht zwingend 
+f.close()
 
-#numOfRigidBodies = 0
-#numOfConstraint = 0
-currentBlockType ="" #
-currentTextBlock = [] #
-searchForObjects = ["RIGID_BODY", "CONSTRAINT"] #
-listOfMbsObjects = [] #
-
+currentBlockType = ""
+currentTextBlock = []
+listOfMbsObjects = []
+search4Objects  = ["RIGID_BODY", "CONSTRAINT"]
 for line in fileContent:
-    if(line.find("$") >= 0):            #new block found
+    if(line.find("$") >= 0):#new block found
         if(currentBlockType != ""):
             if(currentBlockType == "RIGID_BODY"):
-                listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))       # -*-*-*-*-*-*-*
+                listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))
             currentBlockType = ""
-            
-    for type_i in searchForObjects:
+
+    for type_i in search4Objects:
         if(line.find(type_i,1,len(type_i)+1) >= 0):
             currentBlockType = type_i
             currentTextBlock.clear()
             break
+    
     currentTextBlock.append(line)
 
+print(len(listOfMbsObjects))
+
+
+#import/export functionality (for later use in model.py)
 modelObjects = []
 for object in listOfMbsObjects:
     modelObjects.append(object.parameter)
@@ -34,27 +37,12 @@ jDataBase = json.dumps({"modelObjects": modelObjects})
 with open("inputfilereader/test.json", "w") as outfile:
     outfile.write(jDataBase)
 
-
-f = open("inputfilereader/test.json", "r")
+f = open("inputfilereader/test.json","r")
 data = json.load(f)
 f.close()
 
 fds = open("inputfilereader/test.fds","w")
 for mbsObject_i in listOfMbsObjects:
-    mbsObject_i.writeInputFile(fds)
+    mbsObject_i.writeInputfile(fds)
 fds.close()
-
-
-print(len(listOfMbsObjects))            
-print("Nußbaumer Linda")
-
-
-
-
-
-
-
-
-
-
-
+#-------------------------------------------------------
--- inputfilereader/mbsObject.py
+++ inputfilereader/mbsObject.py
@@ -1,8 +1,9 @@
-class mbsObkect:
-    def __init__(self,type,subtype, text, parameter):
+
+class mbsObject:
+    def __init__(self,type,subtype,text,parameter):
         self.__type = type
         self.__subtype = subtype
-        self.parameter = parameter # = ist ref, keine kopie
+        self.parameter = parameter
 
         for line in text:
             splitted = line.split(":")
@@ -12,40 +13,35 @@ class mbsObkect:
                         parameter[key]["value"] = self.str2float(splitted[1])
                     elif(parameter[key]["type"] == "vector"):
                         parameter[key]["value"] = self.str2vector(splitted[1])
-
-    def writeInputFile(self,file):
-        text= []
+    
+    def writeInputfile(self,file):
+        text = []
         text.append(self.__type + " " + self.__subtype + "\n")
         for key in self.parameter.keys():
-            if(self.parameter[key]["type"]== "float"):
-                text.append("\t" + key + " = " + self.float2str(self.parameter[key]["value"]) + "\n")
-            if(self.parameter[key]["type"]== "vector"):
-                text.append("\t" + key + " = " + self.vector2Str(self.parameter[key]["value"]) + "\n")
-        text.append("End" + self.__type + "\n%\n")
+            if(self.parameter[key]["type"] == "float"):
+                text.append("\t"+key+" = "+self.float2str(self.parameter[key]["value"])+"\n")
+            if(self.parameter[key]["type"] == "vector"):
+                text.append("\t"+key+" = "+self.vector2str(self.parameter[key]["value"])+"\n")
+        text.append("End"+self.__type+"\n%\n")
 
         file.writelines(text)
 
-    def str2float(self, inString):
+
+    def str2float(self,inString):
         return float(inString)
-    
-    def float2str(self, inFloat):
+    def float2str(self,inFloat):
         return str(inFloat)
     
-    def vector2Str (self, inVector):
-        return str(inVector[0]) + " , " + str(inVector[1]) + " , " + str(inVector[2])
+    def str2vector(self,inString):
+        return [float(inString.split(",")[0]),float(inString.split(",")[1]),float(inString.split(",")[2])]
+    def vector2str(self,inVector):
+        return str(inVector[0]) + "," + str(inVector[1]) + "," + str(inVector[2])
     
-    def str2vector(self, inString):
-        return [float(inString.split(",")[0]),float(inString.split(",")[1]),float(inString.split(",")[0])]
-
-class rigidBody(mbsObkect):
-    def __init__(self, text):
+class rigidBody(mbsObject):
+    def __init__(self,text):
         parameter = {
             "mass": {"type": "float", "value": 1.},
-            "COG": {"type": "vector", "value": [0.,0.,0.]}  # COG=center of gravity
+            "COG": {"type": "vector", "value": [0.,0.,0.]}
         }
 
-        mbsObkect.__init__(self,"Body","Rigid_EulerParameter", text, parameter)
-
-
-
-                
+        mbsObject.__init__(self,"Body","Rigid_EulerParameter_PAI",text,parameter)
